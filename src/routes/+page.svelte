a69a4560e3b269719186138a29f11e558ca0b43d
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2024-01-25 19:32:06 +0100

    new version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,138 +1,234 @@
-<script>
-</script>
-
 <body>
-	<header>
-		<ul>
-			<li class="logo"><a href="+page.svelte">WE 🖤 WEB</a></li>
-			<li><a href="https://stustu.studio/">Tim Borst</a></li>
-			<li><a href="+ischa.svelte">Ischa Gast</a></li>
-			<li><a href="+page.svelte">workshop</a></li>
-			<li><a href="+page.svelte">workshop</a></li>
+	<section>
+		<link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet" />
+
+		<svg viewBox="0 0 1320 300">
+			<text x="50%" y="50%" dy=".35em" text-anchor="middle"> WEloveWEB </text>
+		</svg>
+	</section>
+
+	<!-- -------------------BACKGROUND-ANIMATION-------------------------- -->
+	<div class="area">
+		<ul class="squares">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
 		</ul>
-	</header>
+	</div>
 
-	<main>
-		<section class="intro">
-			<h1>Code Different</h1>
-		</section>
+	<!-- ----------BLOGPOST------------- -->
 
-		<section class="workshop-1">
-			<h1 class="workshop-1-letter">
-				<a class=" workshop-1-letter" href="https://stustu.studio/">T.B</a>
-			</h1>
-		</section>
-
-		<section class="workshop-2">
-			<h1 class="workshop-2-letter">
-				<a class=" workshop-1-letter" href="https://stustu.studio/">Ischa Gast</a>
-			</h1>
-		</section>
-
-		<section class="workshop-3"></section>
-	</main>
-
-	<footer></footer>
+	
 </body>
 
 <style>
 	* {
-		box-sizing: border-box;
-		margin: 0%;
-	}
-
-	body {
-		background-color: rgb(231, 231, 230);
-	}
-
-	/* ------------------header-------------------- */
-
-	header {
-		position: sticky;
-	}
-
-	ul {
-		list-style-type: none;
 		margin: 0;
-		padding-top: 2em;
-		padding-left: 8em;
-		padding-right: 8em;
-		position: sticky;
+		padding: 0;
+	}
+	body {
+		background: rgb(204, 169, 199);
 	}
 
-	li {
-		display: inline;
-		float: right;
-		padding: 12px;
-		font-family: Courier 'Courier New';
-	}
-
-	a {
-		font-family: 'Courier New', Courier, monospace;
-		display: inline;
-		color: black;
-		font-style: normal;
-		font-weight: bold;
-		text-decoration: none;
-	}
-
-	.logo {
-		float: left;
-	}
-
-	/* ---------------intro---------------- */
-
-	.intro {
-		display: flex;
-		justify-content: center;
+	svg {
+		font-family: 'Russo One', sans-serif;
 		width: 100%;
-		height: 300px;
-		margin-top: 20%;
-		margin-bottom: 300px;
-	}
-
-	h1 {
-		font-size: 12em;
-	}
-
-	/* -------------workshops------------------ */
-
-	.workshop-1 {
-		background: black;
-		width: 1100px;
-		height: 400px;
-		color: white;
-		font-size: 0.5em;
+		height: 100%;
 		display: flex;
-		justify-content: center;
 		align-items: center;
 	}
+	svg text {
+		text-transform: uppercase;
+		animation: stroke 5s infinite alternate;
+		stroke-width: 2;
+		stroke: #a03666;
+		font-size: 140px;
+	}
+	@keyframes stroke {
+		0% {
+			fill: rgba(138, 20, 77, 0);
+			stroke: rgb(160, 54, 132);
+			stroke-dashoffset: 25%;
+			stroke-dasharray: 0 50%;
+			stroke-width: 2;
+		}
+		70% {
+			fill: rgba(95, 20, 138, 0);
+			stroke: rgb(160, 54, 54);
+		}
+		80% {
+			fill: rgba(20, 73, 138, 0);
+			stroke: rgb(153, 54, 160);
+			stroke-width: 3;
+		}
+		100% {
+			fill: rgb(204, 72, 118);
+			stroke: rgba(54, 137, 160, 0);
+			stroke-dashoffset: -25%;
+			stroke-dasharray: 50% 0;
+			stroke-width: 0;
+		}
+	}
+	/* ---------------------------------------------- */
 
-	.workshop-1-letter {
-		color: white;
-		font-size: 5em;
+	section {
 		display: flex;
 		justify-content: center;
-		align-items: center;
+		margin: 250px auto;
+		width: 600px;
+		flex-direction: column;
+		text-align: center;
+		color: #fff;
 	}
 
-	.workshop-2 {
-		background: rgb(103, 2, 2);
-		width: 565px;
-		height: 400px;
-		color: white;
-		font-size: 0.5em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		float: right;
+	.squares {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
 	}
 
-	.workshop-2-letter {
-		color: white;
-		font-size: 2em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
+	.squares li {
+		position: absolute;
+		display: block;
+		list-style: none;
+		width: 20px;
+		height: 20px;
+		background: rgba(255, 255, 255, 0.2);
+		animation: animate 8s linear infinite;
+		bottom: -150px;
+	}
+
+	.squares li:nth-child(1) {
+		left: 25%;
+		width: 80px;
+		height: 80px;
+		animation-delay: 0s;
+	}
+
+	.squares li:nth-child(2) {
+		left: 10%;
+		width: 70px;
+		height: 70px;
+		animation-delay: 0s;
+		animation-duration: 12s;
+	}
+
+	.squares li:nth-child(3) {
+		left: 70%;
+		width: 30px;
+		height: 30px;
+		animation-delay: 2s;
+	}
+
+	.squares li:nth-child(4) {
+		left: 40%;
+		width: 60px;
+		height: 60px;
+		animation-delay: 0s;
+		animation-duration: 18s;
+	}
+
+	.squares li:nth-child(5) {
+		left: 65%;
+		width: 50px;
+		height: 50px;
+		animation-delay: 0s;
+	}
+
+	.squares li:nth-child(6) {
+		left: 75%;
+		width: 110px;
+		height: 110px;
+		animation-delay: 0s;
+	}
+
+	.squares li:nth-child(7) {
+		left: 35%;
+		width: 150px;
+		height: 150px;
+		animation-delay: 4s;
+	}
+
+	.squares li:nth-child(8) {
+		left: 50%;
+		width: 40px;
+		height: 40px;
+		animation-delay: 2s;
+		animation-duration: 30s;
+	}
+
+	.squares li:nth-child(9) {
+		left: 20%;
+		width: 30px;
+		height: 30px;
+		animation-delay: 2s;
+		animation-duration: 25s;
+	}
+
+	.squares li:nth-child(10) {
+		left: 85%;
+		width: 150px;
+		height: 150px;
+		animation-delay: 0s;
+		animation-duration: 11s;
+	}
+
+	.squares li:nth-child(11) {
+		left: 35%;
+		width: 150px;
+		height: 150px;
+		animation-delay: 0s;
+	}
+
+	.squares li:nth-child(12) {
+		left: 50%;
+		width: 70px;
+		height: 70px;
+		animation-delay: 2s;
+		animation-duration: 7s;
+	}
+
+	.squares li:nth-child(13) {
+		left: 20%;
+		width: 70px;
+		height: 70px;
+		animation-delay: 0s;
+		animation-duration: 15s;
+	}
+
+	.squares li:nth-child(14) {
+		left: 85%;
+		width: 150px;
+		height: 150px;
+		animation-delay: 0s;
+		animation-duration: 11s;
+	}
+
+	@keyframes animate {
+		0% {
+			transform: translateY(0px) rotate(0deg);
+			opacity: 1;
+			border-radius: 0;
+		}
+
+		100% {
+			transform: translateY(-1000px) rotate(720deg);
+			opacity: 0;
+			border-radius: 50%;
+		}
 	}
 </style>
