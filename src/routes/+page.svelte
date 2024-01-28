7c40d3b21c99631774c937da43dcf36da7a66f44
anoukbruinn <112856687+anoukbruinn@users.noreply.github.com>
2024-01-28 11:13:01 +0100

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,234 +1,316 @@
 <body>
-	<section>
-		<link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet" />
-
-		<svg viewBox="0 0 1320 300">
-			<text x="50%" y="50%" dy=".35em" text-anchor="middle"> WEloveWEB </text>
-		</svg>
+	<section class="Intro">
+		<div class="">
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<img
+				class="profile"
+				src="src/lib/assets/pictures/profile-picture.png"
+				alt="profile-picture"
+			/>
+		</div>
+		<div class="Title">
+			<h1>I LOVE WEB</h1>
+			<hr />
+			<p>ANOUK BRUIN</p>
+		</div>
 	</section>
 
-	<!-- -------------------BACKGROUND-ANIMATION-------------------------- -->
-	<div class="area">
-		<ul class="squares">
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
-	</div>
+	<section class="ischa">
+		<div class="picture-1">
+			<img class="img-1" src="src/lib/assets/pictures/picture-1.jpeg" alt="im-1" />
+		</div>
 
-	<!-- ----------BLOGPOST------------- -->
+		<div class="Ischa-story">
+			<h2 class=" Ischa-name">ISCHA</h2>
 
-	
+			<div class="paragraph-Ischa">
+				<p>
+					Ischa was een enthousiaste spreker, en zijn presentatie begon met een opvallende
+					vergelijking. Hij wees op het belang van toegankelijkheid op het web door te wijzen op de
+					afhankelijkheid van stemherkenning in het dagelijks leven. "Denk bijvoorbeeld aan de
+					metro," zei Ischa, "De metro's zijn niet toegankelijk als de stemfuncties niet werken."
+					Hij liet een opname horen van een metroaankondiging die niet goed functioneerde, wat tot
+					chaos leidde.
+				</p>
+
+				<p>
+					"Dit is vergelijkbaar met sommige websites die niet toegankelijk zijn," voegde hij eraan
+					toe. Als voorbeeld noemde hij de website van Hema, waarbij een screen reader alleen maar
+					"link link, link link, link link" voorlas, omdat de navigatie niet goed was opgezet.
+				</p>
+
+				<p>
+					Ischa herinnerde het publiek eraan dat Tim Berners-Lee het web had gecreëerd voor
+					iedereen, inclusief blinde, dove, en slechtziende mensen. "We hebben allemaal
+					beperkingen," zei hij. "Sommige mensen hebben tijdelijke beperkingen, zoals
+					slechtziendheid of een gebroken arm."
+				</p>
+
+				<p>
+					Tijdens zijn presentatie liet Ischa leuke slides zien, vaak met cadeautjes om het belang
+					van toegankelijkheid te benadrukken. Hij benadrukte het belang van "hidden tekst" in
+					links, om screen readers van meer informatie te voorzien.
+				</p>
+				<p>
+					Contrast was ook een onderwerp dat ter sprake kwam. Ischa legde uit hoe je contrast kon
+					controleren en benadrukte dat belangrijke knoppen vaak onvoldoende leesbaar waren vanwege
+					slechte contrastverhoudingen. Hij noemde de bonuspagina van Albert Heijn als voorbeeld van
+					een website met slecht contrast.
+				</p>
+				<p>
+					Ischa deelde een persoonlijk verhaal over een collega met een visuele beperking, wiens
+					scherm 200 keer was vergroot en in een omgekeerd contrast was gezet. Deze collega had
+					moeite om de toegankelijkheidsopties op de website van Schiphol te vinden, wat een
+					uitdaging was.
+				</p>
+				<p>
+					Een ander punt dat Ischa benadrukte was het belang van het plaatsen van belangrijke
+					informatie bovenaan een webpagina, vooral op pagina's waar informatie regelmatig
+					verandert. Hij liet zien hoe de website van Schiphol was aangepast om deze wijzigingen
+					door te voeren, zodat reizigers niet de hele website hoefden door te spitten om cruciale
+					informatie te vinden.
+				</p>
+				<p>
+					Ten slotte deelde Ischa een inspirerende video van een jongen die zijn favoriete spelletje
+					kon spelen, ondanks dat hij niet meer kon zien. Deze jongen vertrouwde op zijn gehoor om
+					de aanvallen in het spel te herkennen, zelfs al had hij het spel nooit met zijn eigen ogen
+					gezien.
+				</p>
+
+				<p>
+					Ischa sloot zijn presentatie af met een krachtige boodschap. "Als frontend ontwikkelaars,"
+					zei hij, "moeten we rekening houden met het toegankelijk maken van het wereldwijde web. Zo
+					maken we het web voor iedereen toegankelijk." De studenten in de zaal werden aangemoedigd
+					om het belang van toegankelijkheid in hun toekomstige werk als frontend ontwikkelaars
+					serieus te nemen, en om bij te dragen aan een web dat echt voor iedereen is.
+				</p>
+			</div>
+		</div>
+	</section>
+	<hr class="hr-1" />
+	<section class="Yolijn-story">
+		<div class="Yolijn">
+			<h2 class="Yolijn-name">YOLIJN VAN DER KOLK</h2>
+
+			<div class="paragraph-Yolijn">
+				<p>
+					Yolijn van der Kolk deelde in de We Love Web-blog over design systems, die fungeren als
+					een soort handleiding voor consistent design. Deze systemen zijn als gereedschapskisten
+					voor ontwerpers en ontwikkelaars, met richtlijnen voor verschillende aspecten zoals
+					teksten, microcopy en betrokkenheid van contentschrijvers.
+				</p>
+				<p>
+					Ze benadrukte het belang van een design system dat voortkomt uit inzichten uit
+					gebruikersonderzoek. Yolijn legde de nadruk op het testen met gebruikers, vooral gericht
+					op toegankelijkheid, met een voorbeeld van een test met Stichting Accessibility.
+				</p>
+				<p>
+					Ze illustreerde dit met voorbeelden zoals Sanoma Learning en het design system van ING,
+					waarbij componenten breed toepasbaar zijn zonder de originele huisstijl te verliezen.
+				</p>
+				<p>
+					Het NL Design System, gebruikt door meer dan 350 Nederlandse gemeentes en
+					overheidsinstanties, laat zien hoe samenwerking tussen deze partijen en designers tot
+					succes leidt. Dit systeem minimaliseert dubbel werk door gezamenlijke inspanningen en
+					uitgebreide testen.
+				</p>
+				<p>
+					Yolijn demonstreerde hoe dit systeem wordt toegepast in websites van steden zoals
+					Amsterdam en Utrecht, waarbij ze specifieke elementen zoals breadcrumbs aanhaalde.
+				</p>
+				<p>
+					Een belangrijk aspect hierbij was de EU Public Licence, die niet vereist dat alles open
+					source is en zorgt voor duidelijke CSS-componenten met begrijpelijke namen voor eenvoudig
+					gebruik.
+				</p>
+				<p>
+					Er werd ook nadruk gelegd op design tokens, gebruikt via JSON om consistentie tussen
+					design en CSS te waarborgen. Dit zorgt voor harmonie tussen het ontwerpen en bouwen en
+					heeft aandacht voor toegankelijkheid, zoals het idee om interactieve elementen een
+					minimale grootte te geven voor gebruikers met beperkte motoriek.
+				</p>
+				<p>
+					Haar interesse in toegankelijkheid kwam naar voren door haar "toegankelijkheid bingo" en
+					uitgebreide kennis, hoewel ze dit keer niet diep op dit aspect inging om binnen de
+					presentatietijd te blijven.
+				</p>
+				<p>
+					Tot slot werd waarde gehecht aan het beschermen van intellectueel eigendom, met speciale
+					licenties om het gebruik van huisstijlen te reguleren en kopiëren te voorkomen.
+				</p>
+			</div>
+
+			<div class="picture-2">
+				<img class="img-2" src="src/lib/assets/pictures/picture-2.jpeg" alt="img-2" />
+			</div>
+		</div>
+	</section>
+
+	<hr class="hr-1" />
+
+	<section class="Fenna-story">
+		<div class="Fenna">
+			<h2 class="fenna-name">FENNA DE WILDE</h2>
+			<div class="fenna-paragraph">
+				<p>
+					Fenna de Wilde, een talentvolle creative frondender bij Pakken & Bæck en voorheen werkzaam
+					bij Build in Amsterdam, wierp tijdens haar presentatie "We Love Web" een fascinerende blik
+					op de wereld van webontwikkeling. Met een indrukwekkende reeks projecten en haar
+					betrokkenheid bij de ontwikkeling van de cryptowallet Phantom, bracht ze niet alleen haar
+					werk in de schijnwerpers, maar deelde ze ook inzichten over het integreren van
+					toegankelijkheid in Front-End Development
+				</p>
+
+				<p>
+					De presentatie opende met een visuele reis door Fenna's portfolio, waarbij ze de aandacht
+					vestigde op websites die een bijna kunstzinnige posterstijl omarmen. Een opvallend project
+					was Phantom, waar Fenna bewust koos voor een lichtere en speelsere benadering in
+					tegenstelling tot de donkere en complexe normen van cryptowallets. Ze streeft ernaar om
+					nieuwe crypto-eigenaren uit te nodigen met een begrijpelijke en vriendelijke
+					gebruikersinterface.
+				</p>
+				<p>
+					Een centraal thema dat Fenna aansneed, was de kloof tussen esthetiek en toegankelijkheid
+					op het web. Ze benadrukte het gebrek aan focus op toegankelijkheid in veel bedrijven,
+					waarbij de nadruk vaak ligt op visueel aantrekkelijke animaties. Fenna deelde vervolgens
+					haar toegankelijkheidsstrategieën en de belangrijkste stappen die ze volgt bij het creëren
+					van websites.
+				</p>
+
+				<ul>
+					<li>
+						Focus state voor visueel beperkten: Zichtbare focus state voor gebruikers met visuele
+						beperkingen.
+					</li>
+					<li>
+						Aria-attributen: Gebruik van deze attributen om screen readers te assisteren bij het
+						begrijpen van de content.
+					</li>
+					<li>
+						Toegankelijke carousels: Erkenning van de uitdagingen bij het ontwikkelen van
+						toegankelijke carousels.
+					</li>
+					<li>
+						Modals met <dialog>
+							: Correcte focus state-overdracht naar modals voor een betere gebruikerservaring.
+						</dialog>
+					</li>
+					<li>
+						Kleurcontrast en lettergrootte: Strikte naleving van WCGA-regels om kleurenblindheid te
+						verminderen, samen met aandacht voor lettergrootte en regellengte.
+					</li>
+					<li>
+						Alt-tags: Belang van het beschrijven van afbeeldingen, zelfs bij het gebruik van CMS.
+					</li>
+					<li>Correcte semantiek: Prioriteit voor een vlotte ervaring met screen readers.</li>
+					<li>
+						Skip to content button: Snelle navigatie naar de hoofdinhoud voor gebruikers die de
+						header willen overslaan.
+					</li>
+					<li>
+						Prefers reduced motion button: Optie voor gebruikers om animaties te verminderen of uit
+						te schakelen.
+					</li>
+				</ul>
+
+				<p>
+					Naast haar toegankelijkheidsinzichten onthulde Fenna ook haar benadering van hergebruik in
+					Front-End Development. Ze benadrukte de waarde van bestaande code en ontwerpelementen om
+					efficiëntie te bevorderen.
+				</p>
+				<p>
+					Fenna de Wilde's visie illustreert de kunst van webontwikkeling, waar creativiteit niet
+					alleen wordt gevierd in het ontwerp, maar ook in de inclusiviteit van de digitale
+					ervaring. Haar presentatie benadrukt het belang van een gebalanceerde aanpak in een steeds
+					evoluerend landschap van webontwikkeling.
+				</p>
+			</div>
+		</div>
+	</section>
 </body>
 
 <style>
-	* {
-		margin: 0;
-		padding: 0;
-	}
 	body {
-		background: rgb(204, 169, 199);
+		background-color: white;
 	}
 
-	svg {
-		font-family: 'Russo One', sans-serif;
-		width: 100%;
-		height: 100%;
+	.Intro {
 		display: flex;
-		align-items: center;
 	}
-	svg text {
-		text-transform: uppercase;
-		animation: stroke 5s infinite alternate;
-		stroke-width: 2;
-		stroke: #a03666;
-		font-size: 140px;
-	}
-	@keyframes stroke {
-		0% {
-			fill: rgba(138, 20, 77, 0);
-			stroke: rgb(160, 54, 132);
-			stroke-dashoffset: 25%;
-			stroke-dasharray: 0 50%;
-			stroke-width: 2;
-		}
-		70% {
-			fill: rgba(95, 20, 138, 0);
-			stroke: rgb(160, 54, 54);
-		}
-		80% {
-			fill: rgba(20, 73, 138, 0);
-			stroke: rgb(153, 54, 160);
-			stroke-width: 3;
-		}
-		100% {
-			fill: rgb(204, 72, 118);
-			stroke: rgba(54, 137, 160, 0);
-			stroke-dashoffset: -25%;
-			stroke-dasharray: 50% 0;
-			stroke-width: 0;
-		}
-	}
-	/* ---------------------------------------------- */
 
-	section {
-		display: flex;
-		justify-content: center;
-		margin: 250px auto;
-		width: 600px;
-		flex-direction: column;
+	.profile {
+		width: auto;
+		height: 930px;
+	}
+
+	.Title {
 		text-align: center;
-		color: #fff;
+		padding: 24em;
 	}
 
-	.squares {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		overflow: hidden;
+	h1 {
+		font-size: 40px;
+		width: 300px;
 	}
 
-	.squares li {
-		position: absolute;
-		display: block;
-		list-style: none;
-		width: 20px;
-		height: 20px;
-		background: rgba(255, 255, 255, 0.2);
-		animation: animate 8s linear infinite;
-		bottom: -150px;
+	.ischa {
+		display: flex;
 	}
 
-	.squares li:nth-child(1) {
-		left: 25%;
-		width: 80px;
-		height: 80px;
-		animation-delay: 0s;
+	.Ischa-story {
+		width: 900px;
 	}
 
-	.squares li:nth-child(2) {
-		left: 10%;
-		width: 70px;
-		height: 70px;
-		animation-delay: 0s;
-		animation-duration: 12s;
+	.paragraph-Ischa {
+		float: right;
+		padding-left: 15em;
 	}
 
-	.squares li:nth-child(3) {
-		left: 70%;
-		width: 30px;
-		height: 30px;
-		animation-delay: 2s;
+	.Ischa-name {
+		padding-left: 22em;
 	}
 
-	.squares li:nth-child(4) {
-		left: 40%;
-		width: 60px;
-		height: 60px;
-		animation-delay: 0s;
-		animation-duration: 18s;
+	.img-1 {
+		padding-top: 10em;
+		padding-left: 5em;
 	}
 
-	.squares li:nth-child(5) {
-		left: 65%;
-		width: 50px;
-		height: 50px;
-		animation-delay: 0s;
+	.hr-1 {
+		margin-bottom: 5em;
+		margin-left: 5em;
+		margin-top: 5em;
+		margin-right: 5em;
 	}
 
-	.squares li:nth-child(6) {
-		left: 75%;
-		width: 110px;
-		height: 110px;
-		animation-delay: 0s;
+	.paragraph-Yolijn {
+		width: 880px;
+		float: left;
 	}
 
-	.squares li:nth-child(7) {
-		left: 35%;
-		width: 150px;
-		height: 150px;
-		animation-delay: 4s;
+	.Yolijn-name {
+		padding-left: 14em;
 	}
 
-	.squares li:nth-child(8) {
-		left: 50%;
-		width: 40px;
-		height: 40px;
-		animation-delay: 2s;
-		animation-duration: 30s;
+	.Yolijn-story {
+		display: flex;
+		flex-direction: column;
 	}
 
-	.squares li:nth-child(9) {
-		left: 20%;
-		width: 30px;
-		height: 30px;
-		animation-delay: 2s;
-		animation-duration: 25s;
+	.img-2 {
+		width: 700px;
 	}
 
-	.squares li:nth-child(10) {
-		left: 85%;
-		width: 150px;
-		height: 150px;
-		animation-delay: 0s;
-		animation-duration: 11s;
+	.Fenna {
+		float: center;
 	}
 
-	.squares li:nth-child(11) {
-		left: 35%;
-		width: 150px;
-		height: 150px;
-		animation-delay: 0s;
+	.Fenna-story {
+		width: 1000px;
+		padding-left: 24em;
+		padding-bottom: 5em;
 	}
 
-	.squares li:nth-child(12) {
-		left: 50%;
-		width: 70px;
-		height: 70px;
-		animation-delay: 2s;
-		animation-duration: 7s;
-	}
-
-	.squares li:nth-child(13) {
-		left: 20%;
-		width: 70px;
-		height: 70px;
-		animation-delay: 0s;
-		animation-duration: 15s;
-	}
-
-	.squares li:nth-child(14) {
-		left: 85%;
-		width: 150px;
-		height: 150px;
-		animation-delay: 0s;
-		animation-duration: 11s;
-	}
-
-	@keyframes animate {
-		0% {
-			transform: translateY(0px) rotate(0deg);
-			opacity: 1;
-			border-radius: 0;
-		}
-
-		100% {
-			transform: translateY(-1000px) rotate(720deg);
-			opacity: 0;
-			border-radius: 50%;
-		}
+	.fenna-name {
+		padding-left: 15em;
 	}
 </style>
